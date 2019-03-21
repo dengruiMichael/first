bc337456ba6d2512eeae450f38ff4c08b7fad228
dengruiMichael <40659846+dengruiMichael@users.noreply.github.com>
2019-03-21 14:12:21 +0800

    Update testmysql.php

--- testmysql.php
+++ testmysql.php
@@ -3,9 +3,9 @@
 * Change the value of parameter 3 if you have set a password on the root userid
 * Add port number 3307 in parameter number 5 to use MariaDB instead of MySQL
 *
-*     Utilisation de "mysqli" � la place de "mysql" qui est obsol�te.
-* Changer la valeur du 3e param�tre si vous avez mis un mot de passe � root
-* Ajouter le port 3307 en param�tre 5 si vous voulez utiliser MariaDB
+*     Utilisation de "mysqli" à la place de "mysql" qui est obsolète.
+* Changer la valeur du 3e paramètre si vous avez mis un mot de passe à root
+*哒哒哒哒哒哒多多多多 Ajouter le port 3307 en paramètre 5 si vous voulez utiliser MariaDB
 */
 $mysqli = new mysqli('127.0.0.1', 'root', '', '');
 
